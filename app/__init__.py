6afbc3f1d3f11c919a3730ca7ad2d81c5fdfaffe
Aesha <swatidutta993@gmail.com>
2024-07-09 15:52:18 +0545

    redis

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask, session
 import os
+from redis import Redis, RedisError
 
 
 app = Flask(__name__)
 app.secret_key = os.urandom(20)
+
+# redis_client = Redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
+try:
+    redis = Redis(host='localhost', port=6379)
+    redis.ping()
+    print("Connected to Redis!")
+except RedisError as error:
+    print(f"Failed to connect to Redis: {error}")
 
 @app.route('/')
 def home():
