3dc5c116a9ea93b7f3586c23b1be8ce20f615e37
Aesha <swatidutta993@gmail.com>
2024-07-09 13:28:27 +0545

    new route added

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, session
+import os
 
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 @app.route('/')
 def home():
     return "Hello World!"
+
+@app.route('/visit')
+def visit():
+    if 'visit_count' in session:
+        session['visit_count'] = session.get('visit_count') + 1
+    else:
+        session['visit_count'] = 1
+    
+    return f'You have visited this page {session["visit_count"]} times.'
+
+
+
 
 
 if __name__ == '__main__':
