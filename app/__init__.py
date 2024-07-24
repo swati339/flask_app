ae9bf2cf2180061e89420be9a416b826e7c522d1
Aesha <swatidutta993@gmail.com>
2024-07-24 11:46:57 +0545

    redis implemented

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,10 @@
 from flask import Flask, session
 import os
-from redis import Redis, RedisError
-
+import redis
 
 app = Flask(__name__)
-app.secret_key = os.urandom(20)
-
-# redis_client = Redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
-try:
-    redis = Redis(host='localhost', port=6379)
-    redis.ping()
-    print("Connected to Redis!")
-except RedisError as error:
-    print(f"Failed to connect to Redis: {error}")
+app.secret_key = os.urandom(24)
+cache = redis.Redis(host='localhost', port=6379)
 
 @app.route('/')
 def home():
@@ -24,13 +16,9 @@ def visit():
         session['visit_count'] = session.get('visit_count') + 1
     else:
         session['visit_count'] = 1
-    
+
     return f'You have visited this page {session["visit_count"]} times.'
 
-
-
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+   app.run(host='0.0.0.0', port=8080, debug=True)
 
