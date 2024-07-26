0803ac8ea258fbc26fe8ac3d99dea48f7e91d7e5
Aesha <swatidutta993@gmail.com>
2024-07-26 13:19:44 +0545

    session cookies

--- app/main.py
+++ app/main.py
@@ -1,28 +1,27 @@
-from flask import Blueprint, request, jsonify, current_app, make_response
+from flask import Blueprint, request, jsonify, current_app, make_response, session
 from app.tasks import process_url
 
 bp = Blueprint('main', __name__)
 
 @bp.route('/')
 def index():
-    resp = make_response(jsonify({'message': 'Welcome to the Flask App'}))
-    resp.set_cookie('user_id', '1')  # Set default user ID; ideally, this would be set when user logs in
-    return resp
+     session['user_id'] = '1' 
+     return jsonify({'message': 'Welcome to the Flask App'})
 
 @bp.route('/submit', methods=['GET'])
 def submit_url():
-    user_id = request.args.get('user_id')
+    user_id = session.get('user_id')
     url = request.args.get('url')
     if user_id and url:
         user_key = f"user:{user_id}:urls"
         job = current_app.task_queue.enqueue(process_url, url)
         current_app.redis.lpush(user_key, url)
-        return jsonify({'message': 'URL submitted successfully!', 'url': url, 'job_id': job.get_id()})
+        return jsonify({'message': 'URL submitted successfully!', 'job_id': job.get_id()})
     return jsonify({'message': 'User ID or URL not provided'}), 400
 
 @bp.route('/urls', methods=['GET'])
 def user_urls():
-    user_id = request.args.get('user_id')
+    user_id = session.get('user_id')
     if user_id:
         user_key = f"user:{user_id}:urls"
         urls = current_app.redis.lrange(user_key, 0, -1)
